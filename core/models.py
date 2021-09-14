7aa693a0a1f6695b015a4d82a6989f2c0dc66ced
Thiago <thiagofreitascarneiro@hotmail.com>
2021-09-14 16:22:34 -0300

    adicionando modificações no banco de dados

--- core/models.py
+++ core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Chassi(models.Model):
-    numero = models.CharField('Chassi', max_length=16)
+
+    numero = models.CharField('Chassi', max_length=16, help_text='Maximo 16 caracteres ')
 
     class Meta:
         verbose_name = 'Chassi'
@@ -20,7 +21,7 @@ class Carro(models.Model):
     '''
 
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
-    modelo = models.CharField('Modelo', max_length=30)
+    modelo = models.CharField('Modelo', max_length=30, help_text='Máximo 30 caracteres')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
 
     class Meta:
